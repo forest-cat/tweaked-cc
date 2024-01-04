fccc84678b20cc3a95ff8018b94d900568932665
forest-cat <forest_cat@dismail.de>
2024-01-04 14:03:33 +0100

    fixed crashes

--- playerMonitor/monitor.lua
+++ playerMonitor/monitor.lua
@@ -7,7 +7,12 @@ while true do
     local lengthNum = 0
     local playerList = playerDetector.getOnlinePlayers()
     for i, playerName in pairs(playerList) do
-        if playerName == "wotan_spielt" or playerName == "Forest_cat" then
+        if playerName == "wotan_spielt" then
+            table.remove(playerList, i)
+        end
+    end
+    for i, playerName in pairs(playerList) do
+        if playerName == "Forest_cat" then
             table.remove(playerList, i)
         end
     end
@@ -15,31 +20,41 @@ while true do
     for i, playerName in pairs(playerList) do
         lengthNum = lengthNum + 1
         local playerPos = playerDetector.getPlayerPos(playerName)
-        local distance = math.sqrt((playerPos.x - pcPos.x)^2 + (playerPos.y - pcPos.y)^2 + (playerPos.z - pcPos.z)^2)
         if lengthNum < 8 then
             monitor.setCursorPos(1, i*2)
         else
             monitor.setCursorPos(1, i)
         end
-        monitor.clearLine()
-        if distance > 10000 then monitor.setTextColor(colors.green) end
-        if distance < 10000 and distance > 4000 then monitor.setTextColor(colors.orange) end
-        if distance <= 4000 then monitor.setTextColor(colors.red) end
-        monitor.write(playerName)
-        monitor.setTextColor(colors.lightGray)
-        monitor.write(": ")
-        monitor.setTextColor(colors.white)
-        monitor.write(math.ceil(playerPos.x) .. " ")
-        monitor.write(math.ceil(playerPos.y) .. " ")
-        monitor.write(math.ceil(playerPos.z) .. " ")
-        monitor.setTextColor(colors.lightGray)
-        monitor.write(" | ")
-        if distance > 10000 then monitor.setTextColor(colors.green) end
-        if distance < 10000 and distance > 4000 then monitor.setTextColor(colors.orange) end
-        if distance <= 4000 then monitor.setTextColor(colors.red) end
-        monitor.write(math.floor(distance))
-        monitor.setTextColor(colors.lightGray)
-        monitor.write("m")
+        if playerPos == nil then
+            monitor.clearLine()
+            monitor.setTextColor(colors.white)
+            monitor.write(playerName)
+            monitor.setTextColor(colors.lightGray)
+            monitor.write(": ")
+            monitor.setTextColor(colors.yellow)
+            monitor.write("Position not found")
+        else
+            local distance = math.sqrt((playerPos.x - pcPos.x)^2 + (playerPos.y - pcPos.y)^2 + (playerPos.z - pcPos.z)^2)
+            monitor.clearLine()
+            if distance > 10000 then monitor.setTextColor(colors.green) end
+            if distance < 10000 and distance > 4000 then monitor.setTextColor(colors.orange) end
+            if distance <= 4000 then monitor.setTextColor(colors.red) end
+            monitor.write(playerName)
+            monitor.setTextColor(colors.lightGray)
+            monitor.write(": ")
+            monitor.setTextColor(colors.white)
+            monitor.write(math.ceil(playerPos.x) .. " ")
+            monitor.write(math.ceil(playerPos.y) .. " ")
+            monitor.write(math.ceil(playerPos.z) .. " ")
+            monitor.setTextColor(colors.lightGray)
+            monitor.write(" | ")
+            if distance > 10000 then monitor.setTextColor(colors.green) end
+            if distance < 10000 and distance > 4000 then monitor.setTextColor(colors.orange) end
+            if distance <= 4000 then monitor.setTextColor(colors.red) end
+            monitor.write(math.floor(distance))
+            monitor.setTextColor(colors.lightGray)
+            monitor.write("m")
+        end
     end
     sleep(10)
 end
